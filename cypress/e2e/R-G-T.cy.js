12c2d8eed8bb88a93d49e4daa4933e8418a53054
unknown <Abigail Dadzie>
2024-10-17 10:34:55 -1100

    Update

--- cypress/e2e/R-G-T.cy.js
+++ cypress/e2e/R-G-T.cy.js
@@ -1,62 +1,82 @@
-// import 'cypress-file-upload';
+
 describe('saucedemo', () =>{
 
-    it('login', () =>{
+    // Variables for input data
+    const url = 'https://www.saucedemo.com/';
+    const validUsername = 'standard_user';
+    const validPassword = 'secret_sauce';
+    const invalidUsername = 'Stand_user';
+    const invalidPassword = 'password';
+    const firstName = 'Dome';
+    const lastName = 'Doh';
+    const postalCode = '12345';
 
-        cy.visit("https://www.saucedemo.com/"),
+    it('Verify successful login', () =>{
 
-        cy.url().should("include","saucedemo.com"),
-      //cy.url().should("not.eq","https://www.saucedemo.com/"),
+        cy.visit(url);
 
-      cy.get("#user-name").type("standard_user"),
-      cy.get("#password").type("secret_sauce"),
-      cy.get("#login-button").click();
+        cy.url().should("include", "saucedemo.com");
 
-      cy.url().should('include', '/inventory.html');
-    cy.get('.title').should('have.text', 'Products')
+        // Perform login
+        cy.get("#user-name").type(validUsername);
+        cy.get("#password").type(validPassword);
+        cy.get("#login-button").click();
 
-    cy.get('.inventory_item:first .btn_inventory').click();
+        // Verify successful login by checking if URL contains '/inventory.html'
+        cy.url().should('include', '/inventory.html');
 
-    cy.get('.shopping_cart_badge').should('have.text', '1'); 
+        // Add products to cart
+        cy.get('.title').should('have.text', 'Products');
+        cy.get('.inventory_item:first .btn_inventory').click();
+        cy.get('.shopping_cart_badge').should('have.text', '1');
 
-    cy.get('.product_sort_container').select('az'); // Sort by Name (A to Z)
-    cy.get('.inventory_item:first .inventory_item_name')
-    .should('be.visible')
-    .and('contain.text', 'Sauce');
+        // Sorting and verifying the first item
+        cy.get('.product_sort_container').select('az'); // Sort by Name (A to Z)
+        cy.get('.inventory_item:first .inventory_item_name').should('be.visible').and('contain.text', 'Sauce');
 
-    cy.get('.product_sort_container').select('lohi'); // Sort by Price (Low to High)
-    cy.get('.inventory_item:first .inventory_item_price').should('be.visible'); 
-    
-    cy.get('.shopping_cart_link').click(); // Click on the Cart icon
-    cy.get("#checkout").click();
+        cy.get('.product_sort_container').select('lohi'); // Sort by Price (Low to High)
+        cy.get('.inventory_item:first .inventory_item_price').should('be.visible');
 
-    cy.get("#first-name").type('Dome'); // First name
-    cy.get("#last-name").type('Doh'); // Last name
-    cy.get("#postal-code").type('12345'); // Postal code
-    cy.get("#continue").click(); // Click Continue
+        // Proceed to checkout
+        cy.get('.shopping_cart_link').click(); // Click on the Cart icon
+        cy.get("#checkout").click();
 
-    cy.get('.summary_total_label').should('be.visible'); // Verify total price is visible
-    cy.get("#finish").click();
+        // Fill out checkout information
+        cy.get("#first-name").type(firstName);
+        cy.get("#last-name").type(lastName);
+        cy.get("#postal-code").type(postalCode);
+        cy.get("#continue").click();
 
-    cy.get('.complete-header').should('have.text', 'Thank you for your order!');
+        // Verify the total price and finish order
+        cy.get('.summary_total_label').should('be.visible');
+        cy.get("#finish").click();
 
-    cy.get('#react-burger-menu-btn').click(); // Open the side menu
-    cy.get('#logout_sidebar_link').click();
+        // Verify the thank you message
+        cy.get('.complete-header').should('have.text', 'Thank you for your order!');
 
-    cy.url().should('include', '/');
-    cy.get("#login-button").should('be.visible');
+        // Logout from the application
+        cy.get('#react-burger-menu-btn').click();
+        cy.get('#logout_sidebar_link').click();
+
+        // Verify logout by checking if login button is visible again
+        cy.url().should('include', '/');
+        cy.get("#login-button").should('be.visible');
 
 
     //Negative Test
     it("login with invalid credentials", () =>{
-        cy.visit('https://www.saucedemo.com/'); // Visit the site again
+      
+      cy.visit(url); 
 
-    cy.get("#user-name").type('Stand_user'); // Invalid username
-    cy.get("#password").type('password'); // Invalid password
-    cy.get("#login-button").click();
+      // Perform login with invalid credentials
+      cy.get("#user-name").type(invalidUsername); 
+      cy.get("#password").type(invalidPassword); 
+      cy.get("#login-button").click();
 
-    cy.get('.error-message-container.error').should('be.visible');
-    cy.get('.error-message-container.error').should('contain.text', 'Epic sadface: Username and password do not match any user in this service');
+      // Check for error message
+      cy.get('.error-message-container.error').should('be.visible');
+      cy.get('.error-message-container.error')
+        .should('contain.text', 'Epic sadface: Username and password do not match any user in this service');
     })
 })
 })
